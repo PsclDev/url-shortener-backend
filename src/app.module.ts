313531c97ff02eb2a98c1d6b4d31c9fc1a1486a7
UnRunDead <me@unrundead.dev>
2021-07-26 20:38:52 +0200

    added typeorm to imports and global use

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {typeOrmConfig} from "./database/typeorm.config";
 
 @Module({
-  imports: [UrlModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UrlModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {Logger} from "@nestjs/common";
+import * as config from 'config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const serverConfig = config.get('server');
+    const app = await NestFactory.create(AppModule);
+    const logger = new Logger('Main');
+
+    const port = serverConfig.port;
+    logger.log(`API running on port: ${port}`);
+    await app.listen(port);
 }
+
 bootstrap();
