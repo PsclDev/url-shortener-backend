d785b98e548e50d8a57b4146b04c8f159e8037d3
PsclDev <me@pscl.dev>
2023-03-24 15:01:54 +0100

    chore: removed old project

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! I am a REST-API build on nestJS. Build by github.com/pscldev';
-  }
-}
